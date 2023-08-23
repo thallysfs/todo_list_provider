d874bcb433b440aec4a80d0f5a49b488f0b8b47a
thallysfs <thallys@hotmail.com>
2023-08-23 16:38:47 -0300

    teste

--- lib/app/core/widget/todo_list_field.dart
+++ lib/app/core/widget/todo_list_field.dart
@@ -13,39 +13,44 @@ class TodoListField extends StatelessWidget {
     this.obscureText = false,
     this.suffixIconButton,
   })  : assert(obscureText == true ? suffixIconButton == null : true,
-            'ObscureText não pode ser enviado em conjunto com suffixIconButton'),
+'ObscureText não pode ser enviado em conjunto com suffixIconButton'),
         obscureTextVN = ValueNotifier(obscureText),
         super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return TextFormField(
-      decoration: InputDecoration(
-        labelText: label,
-        labelStyle: TextStyle(
-          fontSize: 15,
-          color: Colors.black,
-        ),
-        border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(30),
-        ),
-        errorBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(30),
-          borderSide: BorderSide(color: Colors.red),
-        ),
-        isDense: true,
-        suffixIcon: this.suffixIconButton ??
-            (obscureText == true
-                ? IconButton(
-                    onPressed: () {},
-                    icon: Icon(
-                      TodoListIcons.eye,
-                      size: 15,
-                    ),
-                  )
-                : null),
-      ),
-      obscureText: obscureText,
-    );
+    return ValueListenableBuilder(
+        valueListenable: obscureTextVN,
+        builder: (_, Value, child) {
+            return TextFormField(
+          decoration: InputDecoration(
+            labelText: label,
+            labelStyle: TextStyle(
+              fontSize: 15,
+              color: Colors.black,
+            ),
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(30),
+            ),
+            errorBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(30),
+              borderSide: BorderSide(color: Colors.red),
+            ),
+            isDense: true,
+            suffixIcon: this.suffixIconButton ??
+                (obscureText == true
+                    ? IconButton(
+                        onPressed: () {},
+                        icon: Icon(
+                          TodoListIcons.eye,
+                          size: 15,
+                        ),
+                      )
+                    : null),
+          ),
+          obscureText: obscureText,
+        );;
+        },
+    )
   }
 }
